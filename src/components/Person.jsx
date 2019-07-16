4e4baade2eebcf2ee42b5330937ac04b3cf6c097
Harrison Booth <harrisonboothh@gmail.com>
2019-07-16 18:06:26 +0100

    refactor person

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 
-const Person = (props) => {
-  return (
-    <img 
-      onDoubleClick={props.onPersonDoubleClick}
-      data-person={JSON.stringify(props.person)}
-      className={(props.disabled) ? 'disabled' : 'person-image'}
-      src={props.person.url} 
-    />
-  )
-}
+const Person = ({ person, onPersonDoubleClick, disabled }) => (
+  <img
+    onDoubleClick={onPersonDoubleClick}
+    data-person={JSON.stringify(person)}
+    className={(disabled) ? 'disabled' : 'person-image'}
+    src={person.url}
+  />
+)
 
 export default Person
